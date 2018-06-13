f74bffb355d033bccf29f56635731d584eee9366
Kevin Gao <kgao@quip.com>
2018-06-13 11:57:48 -0700

    Run prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import { Button } from '@salesforce/design-system-react';
-import logo from './logo.svg';
-import './App.css';
-import PanelExample from './PanelExample';
+import React, {Component} from "react";
+import {Button} from "@salesforce/design-system-react";
+import logo from "./logo.svg";
+import "./App.css";
+import PanelExample from "./PanelExample";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div style={{width: 300}}>
-          <PanelExample />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <div style={{width: 300}}>
+                <PanelExample/>
+            </div>
+        </div>;
+    }
 }
 
 export default App;
