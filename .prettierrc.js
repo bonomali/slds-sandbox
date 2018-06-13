cc3d51d7c22cb4d2c7791fcc4e4a22d89aeef1db
Kevin Gao <kgao@quip.com>
2018-06-13 11:55:11 -0700

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tabWidth: 4,
+    jsxBracketSameLine: true,
+    trailingComma: "es5",
+    bracketSpacing: false,
+    preserveObjectKeyQuotes: true,
+    parensSameLine: true
+};
